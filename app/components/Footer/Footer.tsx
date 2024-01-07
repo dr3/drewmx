2545554913bef26770dca125db11bc65a1a585bc
Drew McMillan <drew@mcmill.co.uk>
2024-01-07 13:13:42 +0000

    🔧 Continue to implement and debug

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,21 +1,30 @@
-import { IconBrandGithub, IconBrandThreads, IconBrandTwitter } from '@tabler/icons-react';
-import './Footer.css'
-import links from '~/config/links';
+import {
+  IconBrandGithub,
+  IconBrandThreads,
+  IconBrandTwitter,
+} from "@tabler/icons-react";
+import "./Footer.css";
+import links from "~/config/links";
 
 const Footer = () => {
-    const year = new Date().getFullYear();
+  const year = new Date().getFullYear();
 
-    return (
-        <footer>
-            <div>
-            © {year} Drew McMillan
-            </div>
-            <div className='footer-links'>
-                <a href={links.github.url}><IconBrandGithub className='footer-icon' /></a>
-                <a href={links.twitter.url}><IconBrandTwitter className='footer-icon' /></a>
-                <a href={links.threads.url}><IconBrandThreads className='footer-icon' /></a>
-            </div>
-        </footer>)
-}
+  return (
+    <footer>
+      <div>© {year} Drew McMillan</div>
+      <div className="footer-links">
+        <a href={links.github.url} aria-label="My Github account">
+          <IconBrandGithub className="footer-icon" />
+        </a>
+        <a href={links.twitter.url} aria-label="My Twitter account">
+          <IconBrandTwitter className="footer-icon" />
+        </a>
+        <a href={links.threads.url} aria-label="My Threads account">
+          <IconBrandThreads className="footer-icon" />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
