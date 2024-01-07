70a78ffbf29727d5fa1fc0c9a713a8e754166441
Drew McMillan <drew@mcmill.co.uk>
2024-01-07 20:57:55 +0000

    🔧 Continue to implement and debug

--- app/config/blog.ts
+++ app/config/blog.ts
@@ -1,18 +1,18 @@
 const blog = [
   {
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/395/200",
     title: "Blog 1",
     date: "July 1, 2021",
     url: "/blog/blog1",
   },
   {
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/395/200",
     title: "Blog 1",
     date: "July 1, 2021",
     url: "/blog/blog2",
   },
   {
-    image: "https://picsum.photos/200/300",
+    image: "https://picsum.photos/395/200",
     title: "Blog 1",
     date: "July 1, 2021",
     url: "/blog/blog3",
